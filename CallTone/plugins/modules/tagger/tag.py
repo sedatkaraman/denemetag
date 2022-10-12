17a159ff599d36bff5d582647c922ffe2dabd5b2
abdullah626 <89883234+abdullah626@users.noreply.github.com>
2022-10-12 21:42:15 +0300

    Update tag.py

--- CallTone/plugins/modules/tagger/tag.py
+++ CallTone/plugins/modules/tagger/tag.py
@@ -177,6 +177,11 @@ async def tcommands(bot: Client, query: CallbackQuery):
                     else:
                         text = f"📢 **{reason}**\n\n{usrtxt}"
                     await bot.send_message(chat, text=text)
+                    c = await message.reply_text(
+            LAN.ZATEN_CALISIYORUM.format(message.from_user.mention)
+        )
+                    await clean_mode(c, message)
+                    return
                     usrnum = 0
                     usrtxt = ""
 
